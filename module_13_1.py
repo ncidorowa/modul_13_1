d81bd482505a0e2002fbbbe2567106a165ec9db1
ncidorowa <166540449+ncidorowa@users.noreply.github.com>
2024-07-23 19:32:31 +0400

    Update module_13_1.py

--- module_13_1.py
+++ module_13_1.py
@@ -5,7 +5,7 @@ import asyncio
 async def start_strongman(name, power):
     print(f'Силач {name} начал соревнования.')
     for i in range(1, 6):
-        await asyncio.sleep((1 / power))
+        await asyncio.sleep(1)
         print(f'Силач {name} поднял {i} шар.')
     print(f'Силач {name} закончил соревнования.')
 
